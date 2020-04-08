a1e48617a8043d9b9fc1eec43d93d041d1a3b3d1
arakaki <arakaki@email.com>
2020-04-08 10:35:13 +0900

    signup login temp finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/top' => 'home#top'
+  get 'users/new' => 'users#new'
+  get 'users/login' => 'users#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
