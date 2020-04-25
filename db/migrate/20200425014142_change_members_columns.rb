faf7befdc309a7d87ff85cb85880900f773bd8f7
arakaki <arakaki@email.com>
2020-04-25 11:14:03 +0900

    add_column password_digest

--- db/migrate/20200425014142_change_members_columns.rb
+++ db/migrate/20200425014142_change_members_columns.rb
@@ -1,5 +1,6 @@
 class ChangeMembersColumns < ActiveRecord::Migration[5.2]
   def change
-    
+    add_column :members, :password_digest, :string
+    remove_column :members, :password, :string
   end
 end
