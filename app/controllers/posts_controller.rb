d6202f2570de7c11690b182456c7f23b1bcdbce8
arakaki <arakaki@email.com>
2020-04-26 10:37:34 +0900

    post search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
 
   def index
-    @posts= Post.all.order(created_at: :desc)
+    @posts= Post.search(params[:search])
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,16 @@ class Post < ApplicationRecord
     def user
         return Member.find_by(id: self.user_id)
       end
+
+
+   def self.search(search) #self.でクラスメソッドとしている
+     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+       Post.where(['title LIKE ?', "%#{search}%"]).order(created_at: :desc)
+    else 
+       Post.all.order(created_at: :desc)
+     end
+   end
+      
       
       
 
